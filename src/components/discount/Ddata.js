45dbdbde1ce50a675e6adce63af09d2a8dc68653
DevHemang <150885858+DevHemang@users.noreply.github.com>
2023-12-01 22:53:33 +0530

    Add files via upload

---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,48 @@
+const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "₹2500",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "₹45000",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "₹20800",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "₹1000",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "₹2000",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "₹5600",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "₹3700",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "₹730",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "₹850",
+  },
+]
+export default Ddata
